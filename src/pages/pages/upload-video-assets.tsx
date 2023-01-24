e3dc6fe23706206e18aa9669facae64ede2055c1
dev_ninja <starhelios94@gmail.com>
2023-01-24 23:02:36 +0500

    feat: upload video assets

---
+++ src/pages/pages/upload-video-assets.tsx
@@ -0,0 +1,19 @@
+import React, { useCallback, useContext, useMemo, useState } from 'react'
+import { useRouter } from 'next/router'
+import { Button, FormControl, FormLabel, Heading, Textarea } from '@chakra-ui/react'
+import UploadVideoAsset from '../../components/pages/UploadVideoAsset'
+import UpdateVideoAsset from '../../components/pages/UpdateVideoAsset'
+
+export default function Upload() {
+  const router = useRouter()
+  const assetId = useMemo(() => (router?.query?.assetId ? String(router?.query?.assetId) : undefined), [router?.query])
+
+  return (
+    <>
+      <Heading as="h3" fontSize="xl" my={4}>
+        Upload Video Assets
+      </Heading>
+      <UploadVideoAsset>{''}</UploadVideoAsset>
+    </>
+  )
+}
